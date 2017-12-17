cc9410c07eeca418b4349abefdbe61a0f2c0a357
Yuki Furukawa <boronngo@gmail.com>
2017-12-17 14:19:28 +0900

    ITP1_1_Cを追加

---
+++ src/ITP1_1_C/Main.java
@@ -0,0 +1,19 @@
+package ITP1_1_C;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		int a = scanner.nextInt();
+		int b = scanner.nextInt();
+		
+		int area = a * b;
+		int perimeter = a * 2 + b * 2;
+		
+		System.out.println(area + " " + perimeter);
+		
+		scanner.close();
+	}
+}
