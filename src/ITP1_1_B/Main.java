ed08de3e0505f07499a32263c5a4e89035975784
Yuki Furukawa <boronngo@gmail.com>
2017-12-17 14:14:31 +0900

    ITP1_1_Bを追加

---
+++ src/ITP1_1_B/Main.java
@@ -0,0 +1,14 @@
+package ITP1_1_B;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		int x = scanner.nextInt();
+		System.out.println(x * x * x);
+		
+		scanner.close();
+	}
+}
