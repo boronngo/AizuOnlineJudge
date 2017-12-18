1e85289cd8eefb0b4eae2b970f1e8944f0592716
Yuki Furukawa <boronngo@gmail.com>
2017-12-18 23:56:00 +0900

    ITP1_4_Cを追加

---
+++ src/ITP1_4_C/Main.java
@@ -0,0 +1,33 @@
+package ITP1_4_C;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		loop: while(scanner.hasNext()) {
+			int a = scanner.nextInt();
+			char op = scanner.next().charAt(0);
+			int b = scanner.nextInt();
+			
+			switch (op) {
+			case '+':
+				System.out.println(a + b);
+				break;
+			case '-':
+				System.out.println(a - b);
+				break;
+			case '*':
+				System.out.println(a * b);
+				break;
+			case '/':
+				System.out.println(a / b);
+				break;
+			case '?':
+				break loop;
+			}			
+		}
+		scanner.close();
+	}
+}
