f9c7b2a889d2e3c4009b3915a45be2fe9e7163eb
Yuki Furukawa <boronngo@gmail.com>
2017-12-20 23:44:13 +0900

    ITP1_5_Bを追加

---
+++ src/ITP1/ITP1_5_B/Main.java
@@ -0,0 +1,36 @@
+package ITP1.ITP1_5_B;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		while(scanner.hasNext()) {
+			int h = scanner.nextInt();
+			int w = scanner.nextInt();
+			
+			if(h == 0 && w == 0) {
+				break;
+			}
+			
+			printFrame(h, w);
+			System.out.println();
+		}
+		
+		scanner.close();
+	}
+	
+	private static void printFrame(int h, int w) {
+		for(int i = 0; i < h; i++) {
+			for(int j = 0; j < w; j++) {
+				if(i == 0 || i == h - 1 || j == 0 || j == w - 1 ) {
+					System.out.print("#");
+				} else {
+					System.out.print(".");
+				}
+			}
+			System.out.println();
+		}
+	}
+}
